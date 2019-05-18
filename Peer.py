0e5beb04a622b34cb021578f89f17487b6ef8446
Walpurgis <sy2328@nyu.edu>
2019-05-18 17:53:23 -0400

    basic framework

--- Peer.py
+++ Peer.py
@@ -1,10 +1,21 @@
 ##http://cs.berry.edu/~nhamid/p2p/framework-python.html
 
+import socket
+import threading
+import time
+import traceback
+import PeerConnection
+
+def btdebug( msg ):
+    """ Prints a messsage to the screen with the name of the current thread """
+    print "[%s] %s" % ( str(threading.currentThread().getName()), msg )
+
+
 class Peer:
 """
 Main function for a peer in a P2P network
 """
-    def __init__( self, maxpeers, serverport, myid=None, serverhost = None ):
+    def __init__( self, maxpeers, serverport, myid=None, serverhost = None, debug = False ):
     	# Grab basic node info
 		self.maxpeers = int(maxpeers)
 		self.serverport = int(serverport)
@@ -21,17 +32,24 @@ Main function for a peer in a P2P network
 		else:  #The peer id will be composed of the host address and port number
 			self.myid = '%s:%d' % (self.serverhost, self.serverport)
 
+		self.peerlock = threading.Lock()  # ensure proper access to
+	                            # peers list (maybe better to use
+	                            # threading.RLock (reentrant))
+
 		# initialize other variables
-		self.debug = 0  
+		self.debug = debug 
 		self.peers = {}  # list (dictionary/hash table) of known peers
 		self.shutdown = False  # used to stop the main loop
-		self.handlers = {}
-		self.router = None
+		self.handlers = {} # [msgtype]( peerconn, msgdata)  how does this work???
+		self.router = None  #Routing function to decide the peer to send msg
 
-
+   	#-------------------------------------------------------------------------
+    # Main functionality
+    #-------------------------------------------------------------------------
     def makeserversocket( self, port, backlog=5 ):
     	"""
-    	create socket and bind to port
+    	Create socket and bind to port
+    	Called in mainloop
     	"""
     	#creates a socket that will communicate using the IPv4 (AF_INET) protocol with TCP (SOCK_STREAM)
 		s = socket.socket( socket.AF_INET, socket.SOCK_STREAM ) 
@@ -41,21 +59,22 @@ Main function for a peer in a P2P network
 		s.listen( backlog ) # backlog: number of incoming connections that can queue up
 		return s
 
-
     def __handlepeer( self, clientsock ):
     	"""
-		Handle new peer connection from clientsock
+		Handle new peer connection recieved from clientsock
+		Called in mainloop
     	"""
+    	self.__debug( 'New child ' + str(threading.currentThread().getName()) )
 		self.__debug( 'Connected ' + str(clientsock.getpeername()) )
 
 		host, port = clientsock.getpeername()
-		peerconn = BTPeerConnection( None, host, port, clientsock, debug=False )
+		peerconn = PeerConnection( None, host, port, clientsock, debug=False )
 		
 		try:
 		    msgtype, msgdata = peerconn.recvdata() #Get some data
 		    if msgtype: msgtype = msgtype.upper()
 
-		    if msgtype not in self.handlers:
+		    if msgtype not in self.handlers:  
 				self.__debug( 'Not handled: %s: %s' % (msgtype, msgdata) )
 		    else:
 				self.__debug( 'Handling peer msg: %s: %s' % (msgtype, msgdata) )
@@ -102,3 +121,195 @@ Main function for a peer in a P2P network
 		self.__debug( 'Main loop exiting' )
 
 		s.close()
+
+
+    def sendtopeer( self, peerid, msgtype, msgdata, waitreply=True ):
+    	"""
+		Use routing function to decide peer to send, and then send it
+    	"""
+		if self.router:
+		    nextpid, host, port = self.router( peerid )
+		if not self.router or not nextpid:  #no function or fail
+		    self.__debug( 'Unable to route %s to %s' % (msgtype, peerid) )
+		    return None
+		return self.connectandsend( host, port, msgtype, msgdata, pid=nextpid,
+					    waitreply=waitreply )
+
+
+    def connectandsend( self, host, port, msgtype, msgdata, pid=None, waitreply=True ):
+		msgreply = []   # list of replies
+		try:
+		    peerconn = PeerConnection( pid, host, port, debug=self.debug )
+		    peerconn.senddata( msgtype, msgdata )
+		    self.__debug( 'Sent %s: %s' % (pid, msgtype) )
+		    
+		    if waitreply:
+				onereply = peerconn.recvdata()
+				while (onereply != (None,None)):  #(msgtype,msgdata)
+				    msgreply.append( onereply )
+				    self.__debug( 'Got reply %s: %s' % ( pid, str(msgreply) ) )
+				    onereply = peerconn.recvdata()
+			    peerconn.close()
+		except KeyboardInterrupt:
+		    raise
+		except:
+		    if self.debug:
+			traceback.print_exc()
+		
+		return msgreply
+
+	def checklivepeers( self ):
+		""" 
+		Attempts to ping all currently known peers in order to ensure that
+		they are still active. Removes any from the peer list that do
+		not reply. This function can be used as a simple stabilizer.
+		"""
+		todelete = []
+		for pid in self.peers:
+		    isconnected = False
+		    try:
+				self.__debug( 'Check live %s' % pid )
+				host,port = self.peers[pid]
+				peerconn = PeerConnection( pid, host, port, debug=self.debug )
+				peerconn.senddata( 'PING', '' )
+				isconnected = True
+		    except:
+				todelete.append( pid )
+		    if isconnected:
+				peerconn.close()
+
+		self.peerlock.acquire()
+		try:
+		    for pid in todelete: 
+				if pid in self.peers: del self.peers[pid]
+		finally:
+		    self.peerlock.release()
+		    
+
+    #-------------------------------------------------------------------------
+    # Settings & Initializations
+    #-------------------------------------------------------------------------
+    def __initserverhost( self ):
+		""" 
+		Attempt to connect to an Internet host in order to determine the
+		local machine's IP address.
+		Called by init if self.serverhost is not provided
+		"""
+		s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		s.connect( ( "www.google.com", 80 ) )
+		self.serverhost = s.getsockname()[0]
+		s.close()
+
+
+    def __debug( self, msg ):
+		if self.debug:
+		    btdebug( msg )
+
+
+    def setmyid( self, myid ):
+		self.myid = myid
+
+
+    def addrouter( self, router ):
+    	"""
+    	router(peername): Routing function that:
+		Given a destination peername(not necessarily in self.peers),
+		decide which known peer msgs should be routed to, to reach(hopefully) the desired peer
+		return: next-peer-id, host, port
+		host, port: IP address of next-peer-id
+		next-peer-id = None if message can't be routed
+		"""
+		self.router = router	
+
+	def addhandler( self, msgtype, handler ):
+		""" Registers the handler for the given message type with this peer """
+		assert len(msgtype) == 4
+		self.handlers[ msgtype ] = handler
+
+
+    def addpeer( self, peerid, host, port ):
+		""" 
+		Adds a peer name and host:port mapping to the known list of peers.
+		"""
+		if peerid not in self.peers and (self.maxpeers == 0 or
+						 len(self.peers) < self.maxpeers):
+		    self.peers[ peerid ] = (host, int(port))
+		    return True
+		else:
+		    return False
+
+    def getpeer( self, peerid ):
+		""" Returns the (host, port) tuple for the given peer name """
+		assert peerid in self.peers    # maybe make this just a return NULL?
+		return self.peers[ peerid ]
+
+
+    def removepeer( self, peerid ):
+		""" Removes peer information from the known list of peers. """
+		if peerid in self.peers:
+		    del self.peers[ peerid ]
+
+   
+    def addpeerat( self, loc, peerid, host, port ):
+		""" 
+		Inserts a peer's information at a specific position in the 
+		list of peers. The functions addpeerat, getpeerat, and removepeerat
+		should not be used concurrently with addpeer, getpeer, and/or 
+		removepeer. 
+		"""
+		self.peers[ loc ] = (peerid, host, int(port))
+
+
+    def getpeerat( self, loc ):
+		if loc not in self.peers:
+		    return None
+		return self.peers[ loc ]
+
+
+    def removepeerat( self, loc ):
+		removepeer( self, loc ) 
+
+    def maxpeersreached( self ):
+		""" 
+		Returns whether the maximum limit of names has been added to the
+		list of known peers. Always returns True if maxpeers is set to
+		0.
+		"""
+		assert self.maxpeers == 0 or len(self.peers) <= self.maxpeers
+		return self.maxpeers > 0 and len(self.peers) == self.maxpeers
+
+    def getpeerids( self ):
+		""" Return a list of all known peer id's. """
+		return self.peers.keys()
+
+
+    def numberofpeers( self ):
+		""" Return the number of known peer's. """
+		return len(self.peers)
+
+    #-------------------------------------------------------------------------
+    # Stabilizer function
+    #-------------------------------------------------------------------------
+    def __runstabilizer( self, stabilizer, delay ):
+		while not self.shutdown:
+		    stabilizer()
+		    time.sleep( delay )
+
+    def startstabilizer( self, stabilizer, delay ):
+		""" 
+		Registers and starts a stabilizer function with this peer. 
+		The function will be activated every <delay> seconds. 
+		"""
+		t = threading.Thread( target = self.__runstabilizer, 
+				      args = [ stabilizer, delay ] )
+		t.start()
+
+
+
+
+
+
+
+
+
+
---
+++ PeerConnection.py
@@ -0,0 +1,97 @@
+import socket
+import struct
+import threading
+import time
+import traceback
+
+
+class PeerConnection:
+    def __init__( self, peerid, host, port, sock=None, debug=False ):
+	# any exceptions thrown upwards
+		self.id = peerid
+		self.debug = debug
+
+		if not sock:  #If client socket hasn't been created
+		    self.socket = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+		    self.socket.connect( ( host, int(port) ) )
+		else:
+		    self.socket = sock
+
+		#create a local file that saves message to sent temporarily
+		self.buffer = self.socket.makefile( 'rw', 0 )  
+
+
+    def __makemsg( self, msgtype, msgdata ):
+		msglen = len(msgdata)
+		msg = struct.pack( "!4sL%ds" % msglen, msgtype, msglen, msgdata )
+		return msg
+
+
+    def senddata( self, msgtype, msgdata ):
+		"""
+		senddata( message type, message data ) -> boolean status
+
+		Send a message through a peer connection. Returns True on success
+		or False if there was an error.
+		"""
+		try:
+		    msg = self.__makemsg( msgtype, msgdata )
+		    self.buffer.write( msg )
+		    self.buffer.flush()
+		except KeyboardInterrupt:
+		    raise
+		except:
+		    if self.debug:
+			traceback.print_exc()
+		    return False
+		return True
+	    
+    def recvdata( self ):
+		"""
+		recvdata() -> (msgtype, msgdata)
+
+		Receive a message from a peer connection. Returns (None, None)
+		if there was any error.
+		"""
+		try:
+		    msgtype = self.buffer.read( 4 )
+		    if not msgtype: return (None, None)
+		    
+	        lenstr = self.buffer.read( 4 )
+		    msglen = int(struct.unpack( "!L", lenstr )[0])  
+		    msg = ""
+
+		    #read until there's message
+		    while len(msg) != msglen:  
+				data = self.buffer.read( min(2048, msglen - len(msg)) )
+				if not len(data):
+				    break
+				msg += data
+
+		    if len(msg) != msglen:
+				return (None, None)
+
+		except KeyboardInterrupt:
+		    raise
+		except:
+		    if self.debug:
+				traceback.print_exc()
+		    return (None, None)
+		return ( msgtype, msg )
+
+    def close( self ):
+		"""
+		Close the peer connection. The send and recv methods will not work
+		after this call.
+		"""
+		self.socket.close()
+		self.socket = None
+		self.buffer = None
+
+
+    def __str__( self ):
+		return "|%s|" % peerid
+
+    def __debug( self, msg ):
+		if self.debug:
+		    btdebug( msg )
